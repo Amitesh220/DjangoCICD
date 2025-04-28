376d146d4da7a5163c8cdbad217fc86ff85284b4
Amitesh Mishra <94803046+Amitesh220@users.noreply.github.com>
2025-04-28 17:04:58 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,7 +1,9 @@
-# Add this if not present
-import os
 
-# Template settings
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
