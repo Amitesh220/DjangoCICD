2d515b6b14b353b5af58f03555d55c8983c24d9a
Amitesh Mishra <94803046+Amitesh220@users.noreply.github.com>
2025-04-28 16:59:18 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello from DjangoCICD minimal project!")
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('api/status/', views.status_api, name='status_api'),
 ]
